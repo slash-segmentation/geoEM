68a0ec2ccc579b38e0564000dcf0f3e50b6a59bf
Jeffrey Bush <jeff@coderforlife.com>
2014-06-20 16:02:14 -0700

    Actually, Flags does support being in nested classes now (using the fully-qualified name when calling FLAGS_DEFN).

--- MedialAxisTransform.cpp
+++ MedialAxisTransform.cpp
@@ -10,11 +10,8 @@ template class MAT_Polygons_3_Facet<MAT>;
 template class CGAL::Triangulation_data_structure_3<TriVertex, TriCell>;
 template class CGAL::Delaunay_triangulation_3<Kernel, TDS>;
 
-namespace detail
-{
-	FLAGS_DEFN(FacetFlags, StableSkeleton, NotStableSkeleton, Boundary, Eaten, InHeap);
-	FLAGS_DEFN(EdgeFlags, Skeleton, StableSkeleton, Visited);
-}
+FLAGS_DEFN(detail::FacetFlags, StableSkeleton, NotStableSkeleton, Boundary, Eaten, InHeap);
+FLAGS_DEFN(detail::EdgeFlags, Skeleton, StableSkeleton, Visited);
 
 void init(Triangulation& T, Polyhedron3* mesh);
 void cleanup(Polyhedron3* mesh);
