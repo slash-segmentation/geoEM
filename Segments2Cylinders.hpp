975bdaa50b9454a7132831254d889caeb268fb4c
Jeffrey Bush <jeff@coderforlife.com>
2017-08-14 16:14:24 -0400

    Impoved Segments2Cylinders utility (allowing one polyhedron to be created from many segments). Also fixed line endings.

--- Segments2Cylinders.hpp
+++ Segments2Cylinders.hpp
@@ -1,4 +1,5 @@
 #include <GeometryTypes.hpp>
 
 Polyhedron3* segment2cylinder(const Segment3& seg, double r=1, size_t n=32);
+Polyhedron3* segments2cylinders_merged(const std::vector<Segment3>& segs, double r=1, size_t n=32);
 std::vector<Polyhedron3*> segments2cylinders(const std::vector<Segment3>& segs, double r=1, size_t n=32);
